600d63d292cd303e2ba68dbd00ddbd861be2b2dd
Andreas <andreas@KoniglisMacBook.fritz.box>
2017-03-02 17:09:53 +0100

    added welcome text

--- js/data/dates.js
+++ js/data/dates.js
@@ -2,11 +2,11 @@ dates = [
     {
         "date": moment(new Date(2017,2,28,20,0)).format('DD.MM.YYYY HH:mm'),
         "title": "Termintitel",
-        "location": "Musterweg 4, 6667 Lummerland"
+        "location": "Gasthaus Knopf, Musterweg 4, 6667 Lummerland"
     },
     {
         "date": moment(new Date(2017,3,12,20,0)).format('DD.MM.YYYY HH:mm'),
         "title": "Termintitel",
-        "location": "Musterweg 4, 6667 Lummerland"
+        "location": "Gasthaus Knopf, Musterweg 4, 6667 Lummerland"
     }
 ]
