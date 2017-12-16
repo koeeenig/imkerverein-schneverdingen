a35bcb7dab7b9a286ded7edd331283c32abb5095
koeeenig <andreas@KoniglisMacBook.fritz.box>
2017-12-16 23:06:26 +0100

    chore: add new dates for 2018

--- js/data/dates.js
+++ js/data/dates.js
@@ -1,41 +1,76 @@
 dates = [
     {
-        "date": moment(new Date(2017,4,12,19,0)).format('DD.MM.YYYY HH:mm'),
+        "date": moment(new Date(2018,0,12,19,0)).format('DD.MM.YYYY HH:mm'),
         "title": "Klönabend",
         "location": "Gasthaus zum Dorfkrug in Insel"
     },
     {
-        "date": moment(new Date(2017,5,9,19,0)).format('DD.MM.YYYY HH:mm'),
+        "date": moment(new Date(2018,0,27,14,0)).format('DD.MM.YYYY HH:mm'),
+        "title": "Jahreshauptversammlung mit Ehrungen",
+        "location": "Gasthaus zum Dorfkrug in Insel"
+    },
+    {
+        "date": moment(new Date(2018,1,3,19,0)).format('DD.MM.YYYY HH:mm'),
+        "title": "Grünkohlessen",
+        "location": "Landgasthaus Wesseloh, Wesseloherstr. 29 (zum Müden)"
+    },
+    {
+        "date": moment(new Date(2018,1,9,19,0)).format('DD.MM.YYYY HH:mm'),
+        "title": "Klönabend mit Film: Im Wunderland der Bienen",
+        "location": "Gasthaus zum Dorfkrug in Insel"
+    },
+    {
+        "date": moment(new Date(2018,1,10,19,0)).format('DD.MM.YYYY HH:mm'),
+        "title": "Heide entkusseln vorgesehen (Schnee ?)",
+        "location": ""
+    },
+    {
+        "date": moment(new Date(2018,2,9,19,0)).format('DD.MM.YYYY HH:mm'),
+        "title": "Klönabend",
+        "location": "Gasthaus zum Dorfkrug in Insel"
+    },
+    {
+        "date": moment(new Date(2018,3,13,19,0)).format('DD.MM.YYYY HH:mm'),
+        "title": "Klönabend mit Film: Die roten Flächen werden grün",
+        "location": "Gasthaus zum Dorfkrug in Insel"
+    },
+    {
+        "date": moment(new Date(2018,4,11,19,0)).format('DD.MM.YYYY HH:mm'),
+        "title": "Klönabend",
+        "location": "Gasthaus zum Dorfkrug in Insel"
+    },
+    {
+        "date": moment(new Date(2018,5,8,19,0)).format('DD.MM.YYYY HH:mm'),
         "title": "Klönabend mit Film: Bienenhaltung in der Rotation",
         "location": "Gasthaus zum Dorfkrug in Insel"
     },
     {
-        "date": moment(new Date(2017,6,14,19,0)).format('DD.MM.YYYY HH:mm'),
+        "date": moment(new Date(2018,6,13,19,0)).format('DD.MM.YYYY HH:mm'),
         "title": "Klönabend",
         "location": "Gasthaus zum Dorfkrug in Insel"
     },
     {
-        "date": moment(new Date(2017,7,11,19,0)).format('DD.MM.YYYY HH:mm'),
-        "title": "Klönabend mit Film über Varroamilbe",
+        "date": moment(new Date(2018,7,10,19,0)).format('DD.MM.YYYY HH:mm'),
+        "title": "Klönabend mit Film: Film über Varroamilbe",
         "location": "Gasthaus zum Dorfkrug in Insel"
     },
     {
-        "date": moment(new Date(2017,8,08,19,0)).format('DD.MM.YYYY HH:mm'),
+        "date": moment(new Date(2018,8,14,19,0)).format('DD.MM.YYYY HH:mm'),
         "title": "Klönabend",
         "location": "Gasthaus zum Dorfkrug in Insel"
     },
     {
-        "date": moment(new Date(2017,9,13,19,0)).format('DD.MM.YYYY HH:mm'),
+        "date": moment(new Date(2018,9,12,19,0)).format('DD.MM.YYYY HH:mm'),
+        "title": "Klönabend mit Film: Unser Honig „Jakobskreuzkraut“",
+        "location": "Gasthaus zum Dorfkrug in Insel"
+    },
+    {
+        "date": moment(new Date(2018,10,09,19,0)).format('DD.MM.YYYY HH:mm'),
         "title": "Klönabend",
         "location": "Gasthaus zum Dorfkrug in Insel"
     },
     {
-        "date": moment(new Date(2017,10,10,19,0)).format('DD.MM.YYYY HH:mm'),
-        "title": "Klönabend",
-        "location": "Gasthaus zum Dorfkrug in Insel"
-    },
-    {
-        "date": moment(new Date(2017,11,08,19,0)).format('DD.MM.YYYY HH:mm'),
+        "date": moment(new Date(2018,11,14,19,0)).format('DD.MM.YYYY HH:mm'),
         "title": "Klönabend",
         "location": "Gasthaus zum Dorfkrug in Insel"
     }
