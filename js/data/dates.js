c74bfe795453e16a18e53656c1e858f22cb231ba
koeeenig <andreas.koenig@icarus-consult.de>
2022-09-29 20:58:18 +0200

    chore: update dates

--- js/data/dates.js
+++ js/data/dates.js
@@ -1,31 +1,6 @@
 dates = [
     {
-        "date": moment(new Date(2021,6,16,19,0)).format('DD.MM.YYYY HH:mm'),
-        "title": "Klönabend",
-        "location": "Gaststätte Löwenbräu, Am Bahndamm 3, 29640 Schneverdingen"
-    },
-    {
-        "date": moment(new Date(2021,7,20,19,0)).format('DD.MM.YYYY HH:mm'),
-        "title": "Klönabend mit Film: Die Varroamilbe",
-        "location": "Gaststätte Löwenbräu, Am Bahndamm 3, 29640 Schneverdingen"
-    },
-    {
-        "date": moment(new Date(2021,8,17,19,0)).format('DD.MM.YYYY HH:mm'),
-        "title": "Klönabend",
-        "location": "Gaststätte Löwenbräu, Am Bahndamm 3, 29640 Schneverdingen"
-    },
-    {
-        "date": moment(new Date(2020,9,15,19,0)).format('DD.MM.YYYY HH:mm'),
-        "title": "Klönabend mit Film: Im Staat aus Wachs und Honig-Bienen",
-        "location": "Gaststätte Löwenbräu, Am Bahndamm 3, 29640 Schneverdingen"
-    },
-    {
-        "date": moment(new Date(2021,10,19,19,0)).format('DD.MM.YYYY HH:mm'),
-        "title": "Klönabend",
-        "location": "Gaststätte Löwenbräu, Am Bahndamm 3, 29640 Schneverdingen"
-    },
-    {
-        "date": moment(new Date(2021,11,17,19,0)).format('DD.MM.YYYY HH:mm'),
+        "date": "Jeden 3 Freitag im Monat um 19 Uhr",
         "title": "Klönabend",
         "location": "Gaststätte Löwenbräu, Am Bahndamm 3, 29640 Schneverdingen"
     }
